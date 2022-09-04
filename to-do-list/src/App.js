777875c800ba1a15ca45780e7f8dc113a8b24f75
Adebanjo401 <108190568+Adebanjo401@users.noreply.github.com>
2022-09-04 20:31:05 +0100

    Update App.js

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -5,8 +5,8 @@ import Todos from './Todos';
 class App extends Component {
   state = {
     todos: [
-      {id: 1, content: 'buy some milk'},
-      {id: 2, content: 'play maro kart'}
+      {id: 1, content: 'Go to the market by 5pm'},
+      {id: 2, content: 'Call the Technician to fix the Fan'}
     ]
   }
   deleteTodo =(id) => {
@@ -21,15 +21,15 @@ class App extends Component {
     todo.id = Math.random();
     let todos = [...this.state.todos, todo]
     this.setState({
-      todos
+      todos  
     })
   }
   render() {
     return (
     <div className="todo-app container">
-      <h1 className="center blue-text">Todo's</h1>
+      <h1 className="center blue-text">MY TO-DO LIST</h1>
       <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
-      <AddTodo addTodo={this.addTodo} />
+      <AddTodo addTodo={this.addTodo}  />
     </div>
     );
   }
